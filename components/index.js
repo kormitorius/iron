5abc9e3c350f068db465c68299b9f626b0d11233
kormitorius <kormitorius@yandex.ru>
2021-11-20 20:13:05 +0300

    components index

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import Article from "./article.js";
+import Contacts from "./contacts.js";
+import Head from "./head.js";
+import PriceList from "./price-list.js";
+import Tags from "./tags.js";
+import App from "./app.js";
+
+const components = {
+    Article,
+    Contacts,
+    Head,
+    PriceList,
+    Tags,
+    App
+}
+
+export default components;
