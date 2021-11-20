531f9f3891504f2aaff5985ff2263c56f651b7b4
kormitorius <kormitorius@yandex.ru>
2021-11-20 21:00:29 +0300

    mobile menu toggle

--- components/head.js
+++ components/head.js
@@ -1,14 +1,36 @@
 import setup from "../setup.js";
 
 const Head = () => {
+    const [open, setOpen] = setup.useState(false);
+
     return setup.html`
         <div class="header">
             <div class="container flex">
                 <img src="logo.png" alt="" />
-                <div class="hamburger"></div>
-                <div class="menu">
+                <div class="hamburger" onclick=${() => setOpen(!open)}></div>
+                <div class=${"menu" + (open ? " open" : "")}>
                     <div class="menu-item">
                         Ремонт
+                        <div class="sub-menu">
+                            <div class="sub-menu__item">
+                                MacBook
+                            </div>
+                            <div class="sub-menu__item">
+                                iPhone
+                            </div>
+                            <div class="sub-menu__item">
+                                iPad
+                            </div>
+                            <div class="sub-menu__item">
+                                iMac
+                            </div>
+                            <div class="sub-menu__item">
+                                iMac mini
+                            </div>
+                            <div class="sub-menu__item">
+                                Apple Watch
+                            </div>
+                        </div>
                     </div>
                     <div class="menu-item">
                         О компании
