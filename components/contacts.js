b3c9f3806342c3d88f11df10ab7687ac3c3dd805
kormitorius <kormitorius@yandex.ru>
2021-11-30 22:13:42 +0300

    call on click

--- components/contacts.js
+++ components/contacts.js
@@ -4,7 +4,7 @@ const Contacts = () => {
     return setup.html`
         <div class="contacts">
             <div class="container contacts__container">
-                <i>Телефон:</i>8-928-850-94-04 <i>Email:</i> ironsapple@gmail.com
+                <i>Телефон:</i><a href="tel:+79288509404">8-928-850-94-04</a> <i>Email:</i> ironsapple@gmail.com
             </div>
         </div>
     `
