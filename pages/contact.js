18e688d3f88e7b3395f8d88e7090d2e496abd925
kormitorius <kormitorius@yandex.ru>
2022-02-20 18:25:20 +0300

    add map

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,7 @@ const ContactPage = () => {
     const { Article, Head, Contacts } = components;
 
     const text = setup.html`
+        <iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=1716684342" width="560" height="400" frameborder="0"></iframe>
         <p>Мы находимся по адресу г. Сочи, ул. Московская д. 5, здание банка ВТБ/Уралсиб. Вход сзади банка - со двора </p>
         <p>Мы работаем каждый день без перерывов и выходных:</p>
         <div>ПН-ПТ: с 9:30 до 20:00</div>
